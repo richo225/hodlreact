303423da40e8a5c678db3d07043b5b4b3c8a452b
Richard Bates <richo225@gmail.com>
2019-11-24 22:18:21 +0000

    Add page header and new logos

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
-
+import Header from './Header';
 import LoginForm from './LoginForm';
 
 const App = () => {
   return (
     <Container>
+      < Header />
       < LoginForm />
     </Container>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Menu, Image, Button } from 'semantic-ui-react';
+
+class Header extends React.Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => { this.setState({activeItem: name}) }
+
+  render () {
+    return (
+      <Menu fixed='top' fluid widths={6} size='huge' inverted>
+        <Menu.Item as='a' header>
+          <Image size='small' src='/hodlmoon/logo_2_transparent.png' style={{ marginRight: '1.5em' }} />
+        </Menu.Item>
+        <Menu.Item
+          as='a'
+          name='home'
+          active={this.state.activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          as='a'
+          name='dashboard'
+          active={this.state.activeItem === 'dashboard'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          as='a'
+          name='portfolio'
+          active={this.state.activeItem === 'portfolio'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          as='a'
+          name='account'
+          active={this.state.activeItem === 'account'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item>
+          <Button inverted color='teal'>Logout</Button>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
+export default Header;
