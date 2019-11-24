66fbdac6352d9704d4fd8cd79a7068086e0b8bc7
Richard Bates <richo225@gmail.com>
2019-11-24 20:25:26 +0000

    Submit values upon logging in

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,49 @@
 import React from 'react';
-import { Button, Form, Grid, Header, Message, Image, Segment } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import { Field, reduxForm } from 'redux-form';
 
-const LoginForm = () => {
-  return(
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'> Login to your account </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input
-              fluid icon='user'
-              iconPosition='left'
-              placeholder='E-mail address'
-            />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
+class LoginForm extends React.Component {
+  onSubmit = (formValues) => {
+    console.log(formValues)
+  }
 
-            <Button color='teal' fluid size='large'> Login </Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  )
+  render () {
+    return(
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='teal' textAlign='center'> Login to your account </Header>
+          <Form size='large' onSubmit={this.props.handleSubmit(this.onSubmit)}>
+            <Segment stacked>
+              <Field
+                component={Form.Input}
+                name='email'
+                placeholder='E-mail address'
+                fluid icon='user'
+                iconPosition='left'
+                type='email'
+                />
+              <Field
+                component={Form.Input}
+                name='password'
+                placeholder='Password'
+                fluid
+                icon='lock'
+                iconPosition='left'
+                type='password'
+                />
+
+              <Button color='teal' fluid size='large'> Login </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Need an account? <a href='#'>Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
-export default LoginForm;
+export default reduxForm({
+  form: 'login'
+})(LoginForm);
