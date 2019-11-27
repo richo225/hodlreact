570fdae0578de0b07f585c79dc25a46e8a0af895
Richard Bates <richo225@gmail.com>
2019-11-27 00:28:11 +0000

    Refactor highlighting navbar using react-router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,35 @@
 import React from 'react';
 import { Menu, Image } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import AuthButton from './AuthButton';
 
 class Header extends React.Component {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => { this.setState({activeItem: name}) }
-
   render () {
     return (
-      <Menu fixed='top' fluid widths={6} size='huge' inverted>
+      <Menu fixed='top' fluid widths={5} size='huge' inverted>
         <Menu.Item as={ Link } to='/' header>
           <Image size='small' src='/logo_2_transparent.png' style={{ marginRight: '1.5em' }} />
         </Menu.Item>
         <Menu.Item
           color='teal'
           as={ Link }
-          name='home'
-          to='/'
-          active={this.state.activeItem === 'home'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          color='teal'
-          as={ Link }
           name='dashboard'
           to='/'
-          active={this.state.activeItem === 'dashboard'}
-          onClick={this.handleItemClick}
+          active={this.props.location.pathname === '/'}
         />
         <Menu.Item
           color='teal'
           as={ Link }
           name='portfolio'
           to='portfolio'
-          active={this.state.activeItem === 'portfolio'}
-          onClick={this.handleItemClick}
+          active={this.props.location.pathname === '/portfolio'}
         />
         <Menu.Item
           color='teal'
           as={ Link }
           name='account'
           to='account'
-          active={this.state.activeItem === 'account'}
-          onClick={this.handleItemClick}
+          active={this.props.location.pathname === '/account'}
         />
         <Menu.Item>
           <AuthButton />
@@ -54,4 +39,4 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+export default withRouter(Header);
