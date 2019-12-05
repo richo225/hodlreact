51e0746120b997caf32d7e9da46fbeeeebe1294c
Richard Bates <richo225@gmail.com>
2019-12-05 14:29:03 +0000

    Render coin icon and symbol inside table

--- src/components/transactions/TransactionList.js
+++ src/components/transactions/TransactionList.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import map from 'lodash/map';
-import { Icon, Table } from 'semantic-ui-react';
+import find from 'lodash/find';
+import { Icon, Image, Table } from 'semantic-ui-react';
 import api from '../../api/dataClient';
 import { Link } from 'react-router-dom';
 
 const TransactionList = () => {
   const [transactions, setTransactions] = useState([])
-  const [coin, setCoin] = useState([])
-
   useEffect( () => { fetchTransactions() }, [] )
 
   const fetchTransactions = async () => {
@@ -16,11 +15,44 @@ const TransactionList = () => {
     setTransactions(data)
   }
 
+  const renderCoinCells = (transactionId) => {
+    const transaction = (find(transactions, { 'id':  transactionId }))
+    const { icon_url, symbol } = transaction.relationships.coin.links
+
+    return(
+      <>
+        <Table.Cell><Image src={icon_url} /></Table.Cell>
+        <Table.Cell>{symbol}</Table.Cell>
+      </>
+    )
+  }
+
+  const renderTransactionCells = (transactionId) => {
+    const transaction = (find(transactions, { 'id':  transactionId }))
+    const {
+      process, exchange, amount, humanised_price,
+      humanised_total_price, created_at, updated_at } = transaction.attributes
+
+    return(
+      <>
+        <Table.Cell>{process}</Table.Cell>
+        <Table.Cell>{exchange}</Table.Cell>
+        <Table.Cell>{amount}</Table.Cell>
+        <Table.Cell>{humanised_price}</Table.Cell>
+        <Table.Cell>{humanised_total_price}</Table.Cell>
+        <Table.Cell>{created_at}</Table.Cell>
+        <Table.Cell>{updated_at}</Table.Cell>
+      </>
+    )
+  }
+
   return (
     <div>
       <Table celled inverted padded selectable>
         <Table.Header>
           <Table.Row textAlign='center'>
+            <Table.HeaderCell></Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
             <Table.HeaderCell>Process</Table.HeaderCell>
             <Table.HeaderCell>Exchange</Table.HeaderCell>
             <Table.HeaderCell>Amount</Table.HeaderCell>
@@ -35,13 +67,8 @@ const TransactionList = () => {
         <Table.Body>
           {map(transactions, (transaction) => (
             <Table.Row key={transaction.id} textAlign='center'>
-              <Table.Cell>{transaction.attributes.process}</Table.Cell>
-              <Table.Cell>{transaction.attributes.exchange}</Table.Cell>
-              <Table.Cell>{transaction.attributes.amount}</Table.Cell>
-              <Table.Cell>{transaction.attributes.humanised_price}</Table.Cell>
-              <Table.Cell>{transaction.attributes.humanised_total_price}</Table.Cell>
-              <Table.Cell>{transaction.attributes.created_at}</Table.Cell>
-              <Table.Cell>{transaction.attributes.updated_at}</Table.Cell>
+              {renderCoinCells(transaction.id)}
+              {renderTransactionCells(transaction.id)}
               <Table.Cell>
                 <Icon name='edit' color='teal' size='big'></Icon>
                 <Icon name='delete' color='orange' size='big'></Icon>
